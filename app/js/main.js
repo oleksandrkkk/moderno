a26f6cb2c86ca264460ce3433875988069fb5853
oleksandrkkk <mr.president0308@gmail.com>
2020-04-27 19:50:24 +0300

    fixed aside bug added circle

--- app/js/main.js
+++ app/js/main.js
@@ -24,12 +24,12 @@
     });
 
     $('.icon-th-list').on('click', function(){
-        $('.product__item').addClass('list');
+        $('.product__page-items .product__item').addClass('list');
         $('.icon-th-list').addClass('active');
         $('.icon-th-large').removeClass('active');
     });
     $('.icon-th-large').on('click', function(){
-        $('.product__item').removeClass('list');
+        $('.product__page-items .product__item').removeClass('list');
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
     });
